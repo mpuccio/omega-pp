68fe5b76d583eb23dd86d4faa685374bc3023e20
Maximiliano Puccio <maximiliano.puccio@cern.ch>
2025-03-06 15:37:04 +0100

    Add download macro

---
+++ download.sh
@@ -0,0 +1,6 @@
+mkdir -p dataMB
+mkdir -p mcMB
+alien_cp alien:/alice/cern.ch/user/a/alihyperloop/outputs/0035/359505/83462/AnalysisResults.root dataMB/AnalysisResults.root
+alien_cp alien:/alice/cern.ch/user/a/alihyperloop/outputs/0035/359505/83462/AO2D.root dataMB/AO2D.root
+alien_cp alien:/alice/cern.ch/user/a/alihyperloop/outputs/0035/356381/82317/AnalysisResults.root mcMB/AnalysisResults.root
+alien_cp alien:/alice/cern.ch/user/a/alihyperloop/outputs/0035/356381/82317/AO2D.root mcMB/AO2D.root
